710388029357ac0cbbd2a175a868ce4dcf9089da
Thanwa <puttazritanwa@gmail.com>
2026-02-15 20:30:35 +0700

    Remove huggingface-hub from requirements

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,4 +1,4 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def load_embeddings():
     return HuggingFaceEmbeddings(
